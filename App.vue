0953573c16f52f93ea0049a4666a720fb1b444a5
qbillym <mucyobilly3@gmail.com>
2025-05-07 13:06:20 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,13 @@
+<template>
+  <UserCard name="Maxime" email="maxime@gmail.com" />
+  <UserCard name="Andy" email="andy@gmail.com" />
+</template>
+<script>
+import UserCard from './components/UserCard.vue';
+export default {
+  name: 'App',
+  components: {
+    UserCard
+  },
+}
+</script>
---
+++ UserCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3>{{ name }}</h3> 
+        <p>Email: {{ email }}</p>  
+        <p>Message: {{ Message }}</p> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserCard',
+    props: {
+        name: { 
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        }
+    },
+    data() { 
+        return {
+            Message: 'Our Website is created using Vue framework.'  
+        };
+    }
+}
+</script>
